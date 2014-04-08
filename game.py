e1ca6685f58298d72a21303e2ac9abb6ba0538fe
user <kis99999@gmail.com>
2014-04-08 16:57:15 +0300

    1

---
+++ game.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import random
+class game2048:
+    arr=[]
+
+    def __init__(self):
+        self.arr = [0]*(4*4)
+        self.add_new_elem(2)
+
+    def invertArr(self,array):
+        return array[::-1]
+
+    ###################################################
+    def shift(self,array,invert):
+        if invert:
+            array=self.invertArr(array)
+        t_array=[]
+        box = 0
+        for x in array:
+            if x==0:
+                continue
+            if x != box:
+                if box:
+                    t_array.append(box)
+                box = x
+            else:
+                t_array.append(box*2)
+                box = 0
+        t_array.append(box)
+        t_array+=[0]*(4-len(t_array))
+        if invert:
+            t_array=self.invertArr(t_array)
+        return t_array
+
+    ###################################################
+
+    def add_new_elem(self,countNewElem=1,clearBeforeAdd=False):
+
+        if(clearBeforeAdd):
+            self.arr=[0]*(4*4)
+
+        sample = random.sample(filter(lambda x:x[1]==0,enumerate(self.arr)),countNewElem)
+        for cell in sample:
+            self.arr[cell[0]] = 2
+        return self.arr
+
+
+    ##################################################
+    BY_ROW = 1
+    BY_COLUMN = 2
+    DIRECTIONS={
+        BY_ROW:lambda row:slice(row*4,row*4+4),
+        BY_COLUMN:lambda col:slice(col,4*4,4)
+    }
+
+    def work(self,dir,invert):
+        for x in range(0,4):
+            self.arr[self.DIRECTIONS[dir](x)]=self.shift(self.arr[self.DIRECTIONS[dir](x)],invert)
+        self.add_new_elem(1)
+        return self.arr
+
--- index_.py
+++ index_.py
@@ -1,68 +1,14 @@
 # -*- coding: utf-8 -*-
-# фыв
-import random
+
+import game
 
 def out(array):
-        for x in range(0,4*4,4):
-            q=array[x:x+4]
-            print(q)
-        print("\n")
+    for x in range(0,4*4,4):
+        q=array[x:x+4]
+        print(q)
+    print("\n")
 
 ###################################################
-
-def invert(arr):
-    return arr[::-1]
-
-###################################################
-def shift(array):
-
-    t_array=[]
-    box = 0
-    for x in array:
-        if x==0:
-            continue
-        if x != box:
-            if box:
-                t_array.append(box)
-            box = x
-        else:
-            t_array.append(box*2)
-            box = 0
-    t_array.append(box)
-    t_array+=[0]*(4-len(t_array))
-
-    return t_array
-
-###################################################
-
-def add_new_elem(arr,countNewElem=1,clearBeforeAdd=False):
-
-    if(clearBeforeAdd):
-        arr=[0]*(4*4)
-
-    sample = random.sample(filter(lambda x:x[1]==0,enumerate(arr)),countNewElem)
-    for cell in sample:
-        arr[cell[0]] = 2
-    return arr
-
-##################################################
-
-BY_ROW = 1
-BY_COLUMN = 2
-DIRECTIONS={
-    BY_ROW:lambda row:slice(row*4,row*4+4),
-    BY_COLUMN:lambda col:slice(col,4*4,4)
-}
-##################################################
-
-
-def work(arr,dir,invert):
-    for x in range(0,4):
-        arr[DIRECTIONS[dir](x)]=shift(arr[DIRECTIONS[dir](x)],invert)
-    arr=add_new_elem(arr,1)
-    return arr
-
-#-----------------------------------------------------------------------
 
 def make_command(func, *args):
     return {
@@ -70,32 +16,33 @@ def make_command(func, *args):
             'args':(args)
             }
 
-arr = [2,0,0,2,
-       0,0,4,2,
-       0,0,0,0,
-       0,0,0,0]
-
-
-event_map = {
-        'w': make_command(work,arr, BY_COLUMN, False),
-        'a': make_command(work,arr, BY_ROW, False),
-        's': make_command(work,arr, BY_COLUMN, True),
-        'd': make_command(work,arr, BY_ROW, True),
-        'n': make_command(add_new_elem,arr, 2, True),
-    }
-
-##################################################
+###################################################
 
 def getKey():
     q=raw_input()
     return q[0]
 
-out(arr)
+###################################################
+
+g2048 = game.game2048()
+
+out(g2048.arr)
+
+event_map = {
+        'w': make_command(g2048.work, 'BY_COLUMN', False),
+        'a': make_command(g2048.work, 'BY_ROW', False),
+        's': make_command(g2048.work, 'BY_COLUMN', True),
+        'd': make_command(g2048.work, 'BY_ROW', True),
+        'n': make_command(g2048.add_new_elem, 2, True),
+    }
+
 k=True
 while k:
     k=getKey()
     if k in event_map:
         func=event_map[k]
+        # print(func)
+        # exit()
         arr=func['func'](*func['args'])
         out(arr)
     else: k=False
